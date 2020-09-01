c72ca19535c981ce64da6846dcad0698cdd5953b
anniewu22 <69912003+anniewu22@users.noreply.github.com>
2020-09-01 13:33:43 -0400

    Add files via upload

--- EscapeRoom.py
+++ EscapeRoom.py
@@ -14,117 +14,114 @@ print("Let's start by looking around and see where we might be.")
 
 # Defining places and objects ------- how to convert to a dictionary ??
 
-def coffee():
-	print("You check the coffee table.")
-	print("There are some mazagines, a coffee mug, and a coaster near the edge.")
-def plant():
-	print("This plant has seen better days...looks like it's been neglected for weeks. Look, it's leaves are all curled and wrinkled up. Not that water will save it now.")
-def desk():
-	print("What a mess..where should I start looking?")
-def magazines():
-	print("Magazine editions are from last week.")
-def coffeeMug():
-	print("There's still some coffee left")
-def coaster():
-	print("Just a coaster")
-def dirt():
-	print("The dirt's all dried up. You feel no resistance as you push your fingers through, until you hit something solid.\n")
-	print("You found a small brown key!")
-def leaves():
-	print("You start to unfold some of the leaves and notice some dark brown spots on some of the leaves.\n")
-	print("You scratch at the spots and it crumbles off.")
-def files():
-	print("--insert something interesting--")
-def drawers():
-	print("It's locked...")
-def coffeeMug2():
-	print("There's still some coffee left")
-
-
-
-def main_menu():
-	print("Where would you like to examine?")
-	print("--------------------")
-
-# Dictionary for main menu options
-printMenu = {"Q": { "function": coffee, "text": "The coffee table"}, 
-"W": { "function": plant, "text": "The pitcher plant"}, 
-"E": { "function": desk, "text": "The computer desk"}
-} 
-# Dictionary for coffee
-coffee_ = {"1": { "function": magazines, "text": "Magazines"},
-"2": { "function": coffeeMug, "text": "Coffee mug"},
-"3": { "function": coaster, "text": "Coaster"},
-"9": { "function": main_menu, "text": "Back to room"}
+examine_dict = {
+"coffee table": {
+	"text": "You check the coffee table. There are some magazines, a coffee mug, and a coaster.",
+	"options": {
+		"1": "magazines", 
+		"2": "coffee mug",
+		"3": "coaster",
+		"4": "return"}	
+	},
+"magazines": {
+	"text": "These editions are from last week",
+	"options": {
+		"1": "collect",
+		"2": "return"}
+	},
+"coffee mug": {
+	"text": "There's still some coffee left",
+	"options": {
+		"1": "collect fingerprint",
+		"2": "return"}
+	},
+"coaster": {
+	"text": "It's just a coaster",
+	"options": {
+		"1": "return"}
+	},
+"plant": {
+	"text": "This plant has seen better days...looks like it's been neglected for weeks. Look, it's leaves are all curled and wrinkled up. Not that water will save it now.",
+	"options": {
+		"1": "dirt",
+		"2": "leaves",
+		"3": "return"}
+	},
+"dirt": {
+	"text": "The dirt's all dried up. You feel no resistance as you push your fingers through, until you hit something solid.",
+	"options": {
+		"1": "collect object",
+		"2": "return"}
+	},
+"leaves": {
+	"text": "You start to unfold some of the leaves and notice some dark brown spots on some of the leaves.",
+	"options": {
+		"1": "Scratch at the leaves",
+		"2": "return" }
+	},
+"desk": {
+	"text": "What a mess..where should I start looking?",
+	"options": {
+		"1": "files",
+		"2": "drawers",
+		"3": "coffee mug",
+		"4": "return"}
+	},
+"files": {
+	"text": "--insert something interesting--",
+	"options": {
+		"1": "read more",
+		"2": "return"}
+	},
+"drawers": {
+	"text": "It's locked...",
+	"options": {
+		"1": "use key",
+		"2": "return"}
+	},
+"coffee mug": {
+	"text": "There's still some coffee left",
+	"options": {
+		"1": "collect fingerprint",
+		"2": "return"}
+	}
 }
-# Dictionary for pitcher plant
-plant_ = {"1": { "function": dirt, "text": "Dirt"},
-"2": { "function": leaves, "text": "Leaves"},
-"9": { "function": main_menu, "text": "Back to room"}
-} 
 	
-# Dictionary for computer desk
-desk_ = {"1": { "function": files, "text": "Scattered files"},
-"2": { "function": drawers, "text": "Drawers"},
-"3": { "function": coffeeMug2, "text": "Coffee mug"},
-"9": { "function": main_menu, "text": "Back to room"}	
+main_ = {"Q": "The coffee table", 
+"W": "The pitcher plant", 
+"E": "The computer desk"
 }
 
+def select():
+	select = input("let's search: ").upper()
+def select1():
+	select1 = input("examine: ")
+def select2():
+	select2 = input("What do you want to do?: ")
+def screen_main():
+	print("Where do you want to search?")
+	print()
+	print(main_)
+	print()
+	print(select())
 
-	
-print(main_menu())
+print(screen_main())
 
-for key in printMenu:
-	print(key + ":\t" + printMenu[key]["text"])
-
-
-check = input("Let's check: ").upper()
-while check == "Q":
-	print(coffee())
-	for key in coffee_:
-		print(key + ":\t" + coffee_[key]["text"])
-	examine = input("Examine: ")
-	if examine == "1":
-		print(magazines())
-		
-	elif examine == "2":
-		print(coffeeMug())
-		
-	elif examine == "3":
-		print(coaster())
-		
-	elif examine == "9":
-		print(main_menu())
-		for key in printMenu:
-			print(key + ":\t" + printMenu[key]["text"])
-print(main_menu)
-
-
-if check == "W":
-	print(plant())
-	for key in plant_:
-		print(key + ":\t" + plant_[key]["text"])
-	exmaine = input("Examine: ")
-	if examine == "1":
-		print(dirt())
-	elif examine == "2":
-		print(leaves())
-
-
-if check == "E":
-	print(desk())
-	for key in desk_:
-		print(key + ":\t" + desk_[key]["text"])
-	examine = input("Examine: ")
-	if examine == "1":
-		print(files())
-	elif examine == "2":
-		print(drawers())
-	elif examine == "3":
-		print(coffeeMug2())
-
-if check == "M":
-	print(main_menu())
+while select == "Q":
+	print(examine_dict["coffee table"]["text"])
+	print(examine_dict["coffee table"]["options"])
+	print(select1())
+	if select1 == "1":
+		print(examine_dict["magazine"]["text"])
+		print(examine["magazine"]["options"])
+		print(select2())
+	elif select2 == "1":
+		print("added to inventory")
+	elif select2 == "2":
+		print(examine_dict["coffee table"]["options"])
+		print(select())
+else:
+	print(screen_main())
 
 
 
